0f1ecf7e0fcae0d5166b55177eaac36ffe2c92ee
rubenmoya <rmoyarodriguez@gmail.com>
2019-10-04 21:16:08 +0200

    feat: block mining

--- src/blockchain/block.ts
+++ src/blockchain/block.ts
@@ -1,3 +1,5 @@
+import { SHA256 } from 'crypto-js'
+
 class Block {
   timestamp: number
   previousHash: string
@@ -25,6 +27,18 @@ class Block {
       '01189998811991197253',
       'So this is how liberty dies…with thunderous applause.',
     )
+  }
+
+  static mine(previousBlock: Block, data: string) {
+    const timestamp = Date.now()
+    const { hash: previousHash } = previousBlock
+    const hash = Block.hash(timestamp, previousHash, data)
+
+    return new Block(timestamp, previousHash, hash, data)
+  }
+
+  static hash(timestamp: number, previousHash: string, data: string): string {
+    return SHA256(`${timestamp}${previousHash}${data}`).toString()
   }
 
   toString(): string {
