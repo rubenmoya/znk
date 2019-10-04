7dd3d6ffd35add1b7f661caeb2dcb1f8c3bd2842
rubenmoya <rmoyarodriguez@gmail.com>
2019-10-04 22:22:47 +0200

    feat: create blockchain class

---
+++ src/blockchain/blockchain.ts
@@ -0,0 +1,20 @@
+import Block from './block'
+
+class Blockchain {
+  blocks: Block[]
+
+  constructor() {
+    this.blocks = [Block.genesis]
+  }
+
+  addBlock(data: string) {
+    const previousBlock = this.blocks[this.blocks.length - 1]
+    const block = Block.mine(previousBlock, data)
+
+    this.blocks.push(block)
+
+    return block
+  }
+}
+
+export default Blockchain
