e180b2b92e762bdd4c066412888b30841a7244dd
rubenmoya <rmoyarodriguez@gmail.com>
2019-10-06 06:28:23 +0200

    feat(p2p): add broadcasting

--- src/service/p2p.ts
+++ src/service/p2p.ts
@@ -4,6 +4,10 @@ import Blockchain from '../blockchain'
 const { P2P_PORT = 5000, PEERS } = process.env
 const peers = PEERS ? PEERS.split(',') : []
 
+const enum MessageType {
+  Blocks = 'blocks',
+}
+
 class P2PService {
   blockchain: Blockchain
   sockets: Websocket[]
@@ -28,6 +32,24 @@ class P2PService {
   onConnection(socket: Websocket) {
     console.log('[ws:socket] connected.')
     this.sockets.push(socket)
+
+    socket.on('message', (message: string) => {
+      const { type, value } = JSON.parse(message)
+      console.log({ type, value })
+    })
+
+    const message = {
+      type: MessageType.Blocks,
+      value: this.blockchain.blocks,
+    }
+
+    socket.send(JSON.stringify(message))
+  }
+
+  broadcast(type: string, value: any) {
+    console.log(`[ws:broadcast] ${type} received...`)
+    const message = JSON.stringify({ type, value })
+    this.sockets.forEach(socket => socket.send(message))
   }
 }
 
